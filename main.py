ea1eb34b6a24a8590507105db4e4394f9bca4948
paarthdani <parth_dani@yahoo.in>
2021-10-30 00:21:45 +0530

    sending new news to telegram

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from helpers import news_scrapper
 from helpers import text_audio_converter
+from helpers import alert_generator
 
 if __name__ == '__main__':
     updated_news_list = news_scrapper.news_scraper()
+    alert_generator.alert_generator(updated_news_list)
     # text_audio_converter.converter()
 
