e9f6fc4765f7881e92344d871af7102096e48bb8
paarthdani <parth_dani@yahoo.in>
2021-10-30 00:16:26 +0530

    developed logic for only adding new news to the csv file

--- helpers/news_scrapper.py
+++ helpers/news_scrapper.py
@@ -1,19 +1,32 @@
 import requests
 from bs4 import BeautifulSoup
-import json
+import csv
 
 
 def news_scraper():
     response = requests.get("https://pulse.zerodha.com/")
     soup = BeautifulSoup(response.content, 'html.parser')
     rev_div = soup.findAll("li", attrs={"class", "box item"})
-    data = {}
+    return add_data(rev_div)
 
-    for i in range(len(rev_div)):
-        data[rev_div[i].find("a").text] = rev_div[i].find("div").text
 
-    with open('../news_data.json', 'a') as file:
-        json_string = json.dumps(data, default=lambda o: o.__dict__, sort_keys=True, indent=2)
-        file.write(json_string)
-    file.close()
+def add_data(rev_div):
+    news_data_list = []
+    result_list = []
+    with open('files/news_data.csv', mode='r', encoding='utf-8', newline='') as news_data:
+        news_reader = csv.reader(news_data, delimiter=',')
+        for row in news_reader:
+            news_data_list.append(row[0])
+
+    with open('files/news_data.csv', mode='a', encoding='utf-8', newline='') as news_data:
+        news_writer = csv.writer(news_data, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        for i in range(len(rev_div)):
+            if rev_div[i].find("a").get('data-id') not in news_data_list:
+                result_list.append([rev_div[i].find("a").get('data-id'),
+                                    rev_div[i].find("a").text,
+                                    rev_div[i].find("a").get('href')])
+                news_writer.writerow([rev_div[i].find("a").get('data-id'),
+                                      rev_div[i].find("a").text,
+                                      rev_div[i].find("a").get('href')])
+        return result_list
 
--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from helpers import news_scrapper
 from helpers import text_audio_converter
 
 if __name__ == '__main__':
-    news_scrapper.news_scraper()
-    text_audio_converter.converter()
+    updated_news_list = news_scrapper.news_scraper()
+    # text_audio_converter.converter()
 
